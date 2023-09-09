816e622f294f8aeb06c47aa64da1bed62b9705eb
t1ep1l0t <alien95111@gmail.com>
2023-09-09 21:04:05 +0300

    update vuex ts support

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from "@/store/store";
+import { store, key } from "@/store/store";
 import i18n from "@/plugins/i18n.config";
 import vuetify from "@/plugins/vuetify.config";
 
@@ -11,7 +11,7 @@ const app = createApp(App)
 
 app
     .use(router)
-    .use(store)
+    .use(store, key)
     .use(i18n)
     .use(vuetify)
     .mount('#app')
